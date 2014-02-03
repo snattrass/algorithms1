77686da62361a807105933a3be732d1eab43cb41
Simon Nattrass <$EMAIL_ADDRESS>
2014-02-03 13:12:03 +0000

    Updating supplied files for this run of the Algorithms 1 course

--- week1/src/PercolationVisualizer.java
+++ week1/src/PercolationVisualizer.java
@@ -20,6 +20,9 @@ import java.awt.Font;
 
 public class PercolationVisualizer {
 
+    // delay in miliseconds (controls animation speed)
+    private static final int DELAY = 100;
+
     // draw N-by-N percolation system
     public static void draw(Percolation perc, int N) {
         StdDraw.clear();
@@ -32,14 +35,17 @@ public class PercolationVisualizer {
         int opened = 0;
         for (int row = 1; row <= N; row++) {
             for (int col = 1; col <= N; col++) {
-                if (perc.isFull(row, col))
+                if (perc.isFull(row, col)) {
                     StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
-                else if (perc.isOpen(row, col))
+                    opened++;
+                }
+                else if (perc.isOpen(row, col)) {
                     StdDraw.setPenColor(StdDraw.WHITE);
+                    opened++;
+                }
                 else
                     StdDraw.setPenColor(StdDraw.BLACK);
                 StdDraw.filledSquare(col - 0.5, N - row + 0.5, 0.45);
-                if (perc.isOpen(row, col)) opened++;
             }
         }
 
@@ -56,16 +62,19 @@ public class PercolationVisualizer {
         In in = new In(args[0]);      // input file
         int N = in.readInt();         // N-by-N percolation system
 
+        // turn on animation mode
+        StdDraw.show(0);
+
         // repeatedly read in sites to open and draw resulting system
         Percolation perc = new Percolation(N);
         draw(perc, N);
+        StdDraw.show(DELAY);
         while (!in.isEmpty()) {
-            StdDraw.show(0);          // turn on animation mode
             int i = in.readInt();
             int j = in.readInt();
             perc.open(i, j);
             draw(perc, N);
-            StdDraw.show(100);        // pause for 100 miliseconds
+            StdDraw.show(DELAY);
         }
     }
 }
