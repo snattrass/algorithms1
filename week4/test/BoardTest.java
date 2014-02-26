07db5b39c43bfd0f4887dd6cb997bcddf1a2001e
Simon Nattrass <$EMAIL_ADDRESS>
2014-02-26 09:26:35 +0000

    Fleshing out hamming and manhattan tests

--- week4/test/BoardTest.java
+++ week4/test/BoardTest.java
@@ -1,16 +1,29 @@
+import junit.framework.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class BoardTest
 {
+    private static final int[][] tiles = {{8, 1, 3}, {4, 0, 2}, {7, 6, 5}};
+    private Board board;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        board = new Board(tiles);
+    }
+
     @Test
     public void testHamming() throws Exception
     {
+        Assert.assertEquals(5, board.hamming());
     }
 
     @Test
     public void testManhattan() throws Exception
     {
+        Assert.assertEquals(10, board.manhattan());
     }
 
     @Test
