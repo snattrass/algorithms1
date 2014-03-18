69add7050d96a43e0b8cdc357a484690a9250846
Simon Nattrass <$EMAIL_ADDRESS>
2014-03-18 20:49:40 +0000

    WIP

--- week5/src/KdTree.java
+++ week5/src/KdTree.java
@@ -26,70 +26,111 @@ public class KdTree
             return;
         }
 
-        Node2D node = findInsertionPoint(p);
-        if (null != node) {
-           return;
+        Node2D node = root;
+        Node2D parent = root;
+        int level = 0;
+        while (null != node) {
+            if (node.point.equals(p)) {     // it already exists
+                return;
+            }
+
+            parent = node;
+            if ((level++ % 2) == 0) {
+                node = (p.x() < node.point.x()) ? node.left : node.right;
+            }
+            else {
+                node = (p.y() < node.point.y()) ? node.left : node.right;
+            }
         }
 
-        Node2D parent = node.parent;
-        if (node.isEvenNode()) {
+        // so we're now at the point where we can insert
+        if (((level - 1) % 2) == 0) {
             if (p.x() < parent.point.x()) {
                 RectHV rect = new RectHV(parent.rect.xmin(), parent.rect.ymin(), parent.point.x(), parent.rect.ymax());
-                parent.left = new Node2D(p, parent.parent, rect);
+                parent.left = new Node2D(p, rect);
             }
             else {
                 RectHV rect = new RectHV(parent.point.x(), parent.rect.ymin(), parent.rect.xmax(), parent.rect.ymax());
-                parent.right = new Node2D(p, parent.parent, rect);
+                parent.right = new Node2D(p, rect);
             }
         }
         else {
             if (p.y() < parent.point.y()) {
                 RectHV rect = new RectHV(parent.rect.xmin(), parent.rect.ymin(), parent.rect.xmax(), parent.point.y());
-                parent.left = new Node2D(p, parent.parent, rect);
+                parent.left = new Node2D(p, rect);
             }
             else {
                 RectHV rect = new RectHV(parent.rect.xmin(), parent.point.y(), parent.rect.xmax(), parent.rect.ymax());
-                parent.right = new Node2D(p, parent.parent, rect);
-            }
-        }
-    }
-
-    private Node2D findInsertionPoint(Point2D point)
-    {
-        Node2D node = root;
-        while (null != node) {
-            if (node.point.equals(point)) {     // it already exists
-                return node;
-            }
-
-            if (node.isEvenNode()) {
-                node = (point.x() < node.point.x()) ? node.left : node.right;
-            }
-            else {
-                node = (point.y() < node.point.y()) ? node.left : node.right;
+                parent.right = new Node2D(p, rect);
             }
         }
 
-        return node;
+        size++;
     }
 
     public boolean contains(Point2D p)              // does the set contain the point p?
     {
-        return null != findInsertionPoint(p);   // did we find a null node to insert in?  If so the tree doesn't contain p
+        Node2D node = root;
+        int level = 0;
+        while (null != node) {
+            if (node.point.equals(p)) {     // it already exists
+                return true;
+            }
+
+            if ((level++ % 2) == 0) {
+                node = (p.x() < node.point.x()) ? node.left : node.right;
+            }
+            else {
+                node = (p.y() < node.point.y()) ? node.left : node.right;
+            }
+        }
+
+        return false;
     }
 
     public void draw()                              // draw all of the points to standard draw
     {
+        draw(root);
+    }
+
+    private void draw(Node2D node)
+    {
+       if (null == node) {
+           return;
+       }
+
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(.005);
+        node.point.draw();
+
+        StdDraw.setPenRadius(.003);
+
+        if (node.isEvenNode()) {
+            StdDraw.setPenColor(StdDraw.RED);
+            node.point.drawTo(new Point2D(node.point.x(), node.rect.ymax()));
+            node.point.drawTo(new Point2D(node.point.x(), node.rect.ymin()));
+
+            draw(node.left);
+            draw(node.right);
+        }
+        else {
+            StdDraw.setPenColor(StdDraw.BLUE);
+            node.point.drawTo(new Point2D(node.rect.xmax(), node.point.y()));
+            node.point.drawTo(new Point2D(node.rect.xmin(), node.point.y()));
+
+            draw(node.left);
+            draw(node.right);
+        }
     }
 
     public Iterable<Point2D> range(RectHV rect)     // all points in the set that are inside the rectangle
     {
         Stack<Point2D> points = new Stack<Point2D>();
-        foo(root, rect, points);
+        range(root, rect, points);
         return points;
     }
 
-    private void foo(Node2D node, RectHV rect, Stack<Point2D> points)
+    private void range(Node2D node, RectHV rect, Stack<Point2D> points)
     {
         if (null == node) {
            return;
@@ -103,15 +144,51 @@ public class KdTree
             points.push(node.point);
         }
 
-        foo(node.left, rect, points);
-        foo(node.right, rect, points);
+        range(node.left, rect, points);
+        range(node.right, rect, points);
     }
 
     public Point2D nearest(Point2D p)               // a nearest neighbor in the set to p; null if set is empty
     {
-        Point2D nearest = null;
+        Stack<Point2D> stack = new Stack<Point2D>();
+        nearest(root, p, stack, 2.0);
+        return stack.pop();
+    }
 
-        return nearest;
+    private void nearest(Node2D node, Point2D point, Stack<Point2D> points, double distance)
+    {
+        if (null == node) {
+            return;
+        }
+
+        if (distance < node.rect.distanceSquaredTo(point))
+            return;
+
+        double d = point.distanceSquaredTo(node.point);
+        if (d < distance) {
+            points.push(node.point);
+        }
+        distance = d;
+
+        if (node.isEvenNode()) {
+            if (point.x() < node.point.x()) {
+                nearest(node.left, point, points, distance);
+                nearest(node.right, point, points, distance);
+            }
+            else {
+                nearest(node.right, point, points, distance);
+                nearest(node.left, point, points, distance);
+            }
+        } else {
+            if (point.y() < node.point.y()) {
+                nearest(node.left, point, points, distance);
+                nearest(node.right, point, points, distance);
+            }
+            else {
+                nearest(node.right, point, points, distance);
+                nearest(node.left, point, points, distance);
+            }
+        }
     }
 
     private class Node2D
@@ -120,13 +197,6 @@ public class KdTree
         {
             this.point = point;
             this.rect = rect;
-        }
-
-        public Node2D(Point2D point, Node2D parent, RectHV rect)
-        {
-            this(point, rect);
-            this.parent = parent;
-            level = parent.level + 1;
         }
 
         public boolean isEvenNode()
